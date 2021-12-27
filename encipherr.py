fbb277ce2fc4d1340709c3d0ef66e317dfef06b5
Oussama Ben Sassi <obensassi.03@gmail.com>
2021-12-27 18:50:38 +0100

    fix lot of bugs

--- encipherr.py
+++ encipherr.py
@@ -1,4 +1,6 @@
-# Encipherr-cli v0.1.0-alpha
+#!/usr/bin/python3
+
+# Encipherr-cli v1.0.0
 
 # Cli version of the open source web app encipherr.Visit https://encipherr.pythonanywhere.com/.
 # Made by Oussama Ben Sassi https://github.com/Oussama1403
@@ -6,41 +8,48 @@
 # Contribute to the development of Encipherr-cli https://github.com/Oussama1403/Encipherr-CLI.
 # Star the repo of Encipherr-cli if you like it :).
 
-
-from cryptography.fernet import Fernet
 import argparse
+from cryptography.fernet import Fernet
+import os
 import sys
 
-parser = argparse.ArgumentParser(description="Encipherr-CLI",usage='\n\npython3 %(prog)s -k yourkey -a E|D -t yourtext OR -f path/to/file\npython3 %(prog)s -genkey to generate a random key.',
-epilog='an issue or a feature request ? contribute to the development of Encipherr-cli https://github.com/Oussama1403/Encipherr-CLI:)')
-
-def ParseArgs():
-    
-    parser.add_argument('-genkey','--generate-key',dest="genkey",action="store_true",help="Generate a random key for encrypting/decrypting.")
-
-    parser.add_argument('-k','-key',dest="key",type=str,help="your key for encrypting/decrypting.")
-    parser.add_argument('-a','-action',dest="action",type=str,help="specify the action [E for encrypt | D for decrypt].")
-    
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-t','-text',dest="text",type=str,help="type your a text after -t arg.") 
-    group.add_argument('-f','-file',dest="filepath",type=str,help="specify the file path after -f arg.") 
-    
-
-    return parser.parse_args()
-
-
-def GenKey():
+def GenKey(args):
     key = Fernet.generate_key()
-    return key.decode()
+    print("your random generated key :\n",key.decode())
 
-def Encrypt(key,args):
-    if args.text == None:
-        print("File encryption is not implemented for the moment.")
-        print("look for updates at https://github.com/Oussama1403/Encipherr-CLI.")
+def is_valid_file(path):
+    if not os.path.isfile(path):
+        return False
+    else:
+        return True
+
+def Encrypt(args):
+    key = args.key
+    if args.mode in ['file','FILE']:
+        try:
+            path = " ".join(args.input)
+            print("specified path:",path)
+            fernet = Fernet(key)
+            
+            if is_valid_file(path):
+                with open(path,'rb') as f:
+                    data = f.read()
+            else:
+                print("Error in provided path !") 
+                sys.exit(1)       
+            
+            encryptedfile = fernet.encrypt(data)
+            with open(path,'wb') as f:
+                f.write(encryptedfile)
+            print("the file at ",path," is encrypted")    
+        except:
+            print("Error in Encryption!, Possible problems : Invalid Key or Invalid input")    
+
     else:
         try:
             #text encryption mode
-            value = args.text
+            value = " ".join(args.input)
+            print("value",value)
             fernet = Fernet(key)
             plaintext = value.encode()
             encryptedtext = fernet.encrypt(plaintext)
@@ -49,16 +58,35 @@ def Encrypt(key,args):
             print()
             print(encryptedtext.decode())
         except:
-            print("Error in Encryption!, Possible problems : Key Not Found or Invalid Key")    
+            print("Error in Encryption!, Possible problems : Invalid Key or Invalid input")    
 
-def Decrypt(key,args):
-    if args.text == None:
-        print("File decryption is not implemented for the moment.")
-        print("look for updates at https://github.com/Oussama1403/Encipherr-CLI.")
+def Decrypt(args):
+    key = args.key
+    if args.mode in ['file',"FILE"]:
+        try:
+            path = " ".join(args.input)
+            print("specified path:",path)
+            fernet = Fernet(key)
+            
+            if is_valid_file(path):
+                with open(path,'rb') as f:
+                    data = f.read()
+            else:
+                print("Error in provided path !") 
+                sys.exit(1)
+            
+            decryptedfile = fernet.decrypt(data)
+            with open(path,'wb') as f:
+                f.write(decryptedfile)
+            print("the file at ",path," is decrypted") 
+        except:
+            print("Error in Decryption!, Possible problems : Invalid Key or Invalid input")      
+   
+    
     else:
         try:
             #text decryption mode
-            value = args.text
+            value = " ".join(args.input)
             fernet = Fernet(key)
             plaintext = value.encode()
             decryptedtext = fernet.decrypt(plaintext)
@@ -67,29 +95,34 @@ def Decrypt(key,args):
             print()
             print(decryptedtext.decode())
         except:
-            print("Error in Decryption!, Possible problems : Key Not Found or Invalid Key")      
+            print("Error in Decryption!, Possible problems : Invalid Key or Invalid input")      
 
-def main():
-    args = ParseArgs()
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description="Encipherr-CLI 1.0.0 (https://github.com/Oussama1403/Encipherr-CLI)",
+epilog='Exemple:\n\n python3 encipherr.py encrypt ueVEtmfzr8d... TEXT encipherr is awesome!\n ---or---\n python3 encipherr.py decrypt ueVEtmfzr8d... FILE path/to/file\n\n *if you don\'t have a key:\n python3 encipherr.py genkey\n\nan issue or a feature request ? contribute to the development of Encipherr-cli https://github.com/Oussama1403/Encipherr-CLI:)')
+subparsers = parser.add_subparsers()
+
+GenKey_parser = subparsers.add_parser('genkey',help="Generate a random key for encrypting/decrypting.")
+GenKey_parser.set_defaults(func=GenKey)
+ 
+Encrypt_parser = subparsers.add_parser('encrypt',help="encrypt key mode input")
+Encrypt_parser.add_argument('key',type=str,help="your key for encrypting/decrypting.")
+Encrypt_parser.add_argument('mode',type=str,choices=['text','TEXT','file','FILE'],help="TEXT or FILE")
+Encrypt_parser.add_argument('input',type=str,nargs="+",help="A text if in text mode or path/to/file if in file mode")
+Encrypt_parser.set_defaults(func=Encrypt)
+
+
+Decrypt_parser = subparsers.add_parser('decrypt',help="decrypt key mode input")
+Decrypt_parser.add_argument('key',type=str,help="your key for encrypting/decrypting.")
+Decrypt_parser.add_argument('mode',type=str,choices=['text','TEXT','file','FILE'],help="TEXT or FILE")
+Decrypt_parser.add_argument('input',type=str,nargs="+",help="A text if in text mode or path/to/file if in file mode")
+Decrypt_parser.set_defaults(func=Decrypt)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
     #if no args,show help and exit.
     if len(sys.argv)==1:
        parser.print_help(sys.stderr)
        sys.exit(1)
-    #generate a key
-    if args.genkey == True:
-        key = GenKey()
-        print(key)
-    #call encrypt/decrypt functions
-    if args.action == "E":
-        key = args.key
-        Encrypt(key,args)  
-    elif args.action == "D":
-        key = args.key
-        Decrypt(key,args)
-    else:
-        pass     
-
-#start program      
-main()
-
-
+    
+    args.func(args)
